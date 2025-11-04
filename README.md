32a52f244f9cbd36881d7cbe66cf8d6563fc8091
nlankwei5 <lampteyebenezer220@gmail.com>
2025-11-04 11:13:37 +0000

    Bootstrapped project and also built telemetry model for insights app

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# AI-F1-Intelligence-System
+# AI-F1-Intelligence-System (ApexFlow)
+
+This is an AI-driven telemetry and analytics backend inspired by Formula 1 race intelligence systems.
+It ingests, processes, and analyzes high-frequency data streams in real time — and delivers both technical and business insights through Grafana and Power BI dashboards.
+
+🚀 Project Overview
+It  provides a unified platform for:
+
+- Real-time data ingestion from live or simulated F1 telemetry (via FastF1)
+
+- Stream and asynchronous processing using Celery and Faust
+
+- AI-powered analytics for performance insights using OpenAI
+
+- Dual visualization for both engineers (Grafana) and executives (Power BI)
+
+This project uses Formula 1 data as a prototype, but the architecture is industry-agnostic, designed to scale into finance, logistics, energy, or manufacturing telemetry systems.
---
+++ ingestion/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+import uuid 
+
+
+
+class TelemetryData(models.Model):
+    driver_name = models.CharField(max_length=100)
+    team = models.CharField(max_length=50)
+    circuit_name = models.CharField(max_length=100)
+    lap = models.IntegerField(default=0)
+    speed = models.FloatField()
+    throttle = models.FloatField()
+    brake = models.FloatField()
+    gear = models.IntegerField(default=0)
+    rpm = models.IntegerField(default=0)
+    drs = models.BooleanField(default=False)
+    time = models.DateTimeField(auto_now=True)
+    session_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.team} - {self.driver_name}"
+
+    class Meta:
+        indexes = [      
+            models.Index(fields=["driver_name"]),       
+            models.Index(fields=["team"]),       
+            models.Index(fields=["circuit_name"]),       
+        ]
+
+
+
+class EventLog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    event_type = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at =  models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
