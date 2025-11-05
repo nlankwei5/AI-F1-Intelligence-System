9aab2ee3610365944d1c58eaee7d542ef6357a4b
nlankwei5 <lampteyebenezer220@gmail.com>
2025-11-05 11:21:14 +0000

    update models.py

--- ingestion/models.py
+++ ingestion/models.py
@@ -14,8 +14,8 @@ class TelemetryData(models.Model):
     gear = models.IntegerField(default=0)
     rpm = models.IntegerField(default=0)
     drs = models.BooleanField(default=False)
-    time = models.DateTimeField(auto_now=True)
-    session_type = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now_add=True)
+    session_type = models.CharField(max_length=100, blank=True,  null =True)
 
     def __str__(self):
         return f"{self.driver_name}: {self.circuit_name} - {self.lap}"
@@ -31,9 +31,10 @@ class TelemetryData(models.Model):
 
 class EventLog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    telemetry = models.ForeignKey(TelemetryData, on_delete=models.CASCADE, null=True, blank=True)
     event_type = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    created_at =  models.DateTimeField(auto_now=True)
+    created_at =  models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.id}"
