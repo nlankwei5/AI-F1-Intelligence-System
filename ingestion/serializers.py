efe1c64996398aab84e0bf602ac89d703af573ce
nlankwei5 <lampteyebenezer220@gmail.com>
2025-11-07 13:57:15 +0000

    Update serializer

--- ingestion/serializers.py
+++ ingestion/serializers.py
@@ -7,7 +7,7 @@ class TelemetryDataSerializer(serializers.ModelSerializer):
     class Meta: 
         model = TelemetryData
         fields = ['id', 'driver_name', 'circuit_name', 'lap', 'speed', 'throttle', 'brake', 'gear', 'rpm', 'drs', 'session_type', 'time']
-        read_only_fields= ['time ']
+        read_only_fields= ['time']
 
 
 
