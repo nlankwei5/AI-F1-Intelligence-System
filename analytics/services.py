3c421f1b105508ca4221d14543a3a3a82980f023
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2026-01-15 14:32:01 +0000

    Update db to use sqlite for dev environment as postgres is messy on MacOS

--- analytics/services.py
+++ analytics/services.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key= os.getenv('OPEN_AI_KEY'))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def analyze_with_gpt(prompt):
     """
--- core/settings.py
+++ core/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
